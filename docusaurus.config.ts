7f97570ddc79dd49400784b7852926b97402e596
Benoit <benoitmbole4@gmail.com>
2026-06-15 09:39:03 +0200

    tagline and title changed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,8 +9,8 @@ dotenvconfig();
 const blogEnabled = Boolean(process.env.BLOG_ENABLED === 'true')
 
 const config: Config = {
-  title: 'DSO Live Demo Docs',
-  tagline: 'Dinosaurs are cool',
+  title: 'Portfolio',
+  tagline: 'Benoit Mbole Ateba, DevSecOps Enthusiast with a passion for details and efficiency',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
